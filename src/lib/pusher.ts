58c2da95e8673ee9bd8dfed8b7a286e4239dafbe
Lamiaa Gabriel <lamiaamonaem6@gmail.com>
2023-08-31 17:45:29 +0300

    first commit

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -1,7 +1,5 @@
 import PusherServer from "pusher"
 import PusherClient from "pusher-js"
-import dotenv from "dotenv"
-dotenv.config()
 
 if (!process.env.PUSHER_APP_ID) throw Error("PUSHER_APP_ID IS MISSING!")
 if (!process.env.NEXT_PUBLIC_PUSHER_APP_KEY)
@@ -11,11 +9,6 @@ if (!process.env.PUSHER_APP_CLUSTER)
   throw Error("PUSHER_APP_CLUSTER IS MISSING!")
 
 export const pusherServer = new PusherServer({
-  // appId: "1662010",
-  // key: "219244c47391d12fe28c",
-  // secret: "806be4329afb41a0cfe2",
-  // cluster: "eu",
-
   appId: process.env.PUSHER_APP_ID,
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
   secret: process.env.PUSHER_APP_SECRET,
@@ -26,7 +19,6 @@ export const pusherServer = new PusherServer({
 export const pusherClient = new PusherClient(
   process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
   {
-    // cluster: "eu",
     cluster: process.env.PUSHER_APP_CLUSTER,
   }
 )
